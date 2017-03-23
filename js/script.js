391eab03fbd9cc7c30de8771443f3a2fc15ed260
cwryd <cryder9898@gmail.com>
2017-03-23 11:22:11 -0400

    added placeholder bootstrap modal to choose X or O

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
     
+    var X_MARK = 'X';
+    var O_MARK = 'O';
+    var player, ai;
+    
+    $('.square').click(function () {
+        $(this).text('X');
+    });
     
 });
