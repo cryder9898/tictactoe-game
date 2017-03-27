8652c1271e49cb00e46137bcec66803870ea1bb2
cwryd <cryder9898@gmail.com>
2017-03-27 09:13:15 -0400

    added reset, playerMove, aiMove function. Also newGame functionality.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,66 @@
 $(document).ready(function () {
   "use strict";
-
-  var X_MARK = 'X',
-      O_MARK = 'O',
-      player,
-      ai;
+  var player, ai,
+      isPlayerTurn;
   
-  function setPlayer(char) {
-    player = char;
+  function reset() {
+    player = null;
+    ai = null;
+    isPlayerTurn = null;
+    $('.square').text('');
+  }
+  
+  // chooses either 1 or zero to decide who plays first
+  function pickRandPlayer() {
+    return Math.floor(Math.random() * 2);
+  }
+  
+  function aiMove() {
+    
+    playerMove();
+  }
+  
+  function playerMove() {
+    $('.square').click(function () {
+        $(this).text(player);
+        aiMove();
+    });
   }
   
   function newGame() {
+    $('#start').text('Start Over');
+    isPlayerTurn = pickRandPlayer();
+    console.log(isPlayerTurn + " goes first");
+    
+    if (isPlayerTurn === 0) {
+      playerMove();
+    } 
+    else {
+      aiMove();
+    }
     
   }
-    
-  $('.square').click(function () {
-      $(this).text('X');
+  
+  // starts the game
+  $('#play').click(function () {
+    newGame();
+  });
+  
+  
+  // player picks what token to play with
+  $('.btn-lg').click(function () {
+    if ($(this).attr('id') === 'X') {
+      player = 'X';
+    } else {
+      player = 'O';
+    }
+    console.log("you choose: " + player);
+  });
+  
+  $('#start').click(function () {
+    if ($('#start').text() === 'Start Over') {
+      reset();
+    }
   });
     
 });
