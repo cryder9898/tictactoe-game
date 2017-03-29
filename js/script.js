077df32fbc75838aff3f3dc8aae0d890f9c4d401
cwryd <cryder9898@gmail.com>
2017-03-28 20:42:18 -0400

    added simple ai to choose move

--- js/script.js
+++ js/script.js
@@ -1,66 +1,128 @@
 $(document).ready(function () {
   "use strict";
-  var player, ai,
-      isPlayerTurn;
+  var board = [],
+      player = '', 
+      ai = '';
   
-  function reset() {
-    player = null;
-    ai = null;
-    isPlayerTurn = null;
-    $('.square').text('');
-  }
-  
-  // chooses either 1 or zero to decide who plays first
-  function pickRandPlayer() {
+  // chooses either 1 or 0 to decide who plays first
+  function getFirstPlay() {
     return Math.floor(Math.random() * 2);
   }
   
-  function aiMove() {
+  function isWin() {
     
-    playerMove();
+    //checks rows for same letter
+    for (var i = 0; i <= 6; i = i + 3) {
+      if (board[i] !== '' && board[i] === board[i + 1] && board[i] === board[i + 2]) {
+        return true;
+      }
+    }
+    
+    //checks cols for same letter
+    for (var i = 0; i <= 2; i++) {
+      if (board[i] !== '' && board[i] === board[i + 3] && board[i] === board[i + 6]) {
+        return true;
+      }
+    }
+    
+    //checks diagonals for same letter
+    if (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) {
+      return true;
+    }
+    
+    if (board[2] !== '' && board[2] === board[4] && board[2] === board[6]) {
+      return true;
+    }
+    
+    return false;
   }
   
-  function playerMove() {
-    $('.square').click(function () {
-        $(this).text(player);
-        aiMove();
-    });
+  function reset() {
+    // make 1 dimensional board of length 9
+    board = [];
+    for (var i = 0; i < 9; i++) {
+      board.push('');
+    }
+    
+    //reset all values
+    //player = '';
+    //ai = '';
+    $('.square').text('');
+    $('#win').text('');
   }
   
-  function newGame() {
-    $('#start').text('Start Over');
-    isPlayerTurn = pickRandPlayer();
-    console.log(isPlayerTurn + " goes first");
-    
-    if (isPlayerTurn === 0) {
-      playerMove();
-    } 
-    else {
+  // starts the game
+  function playGame() {
+    $('#start').text('Start Over');    
+    if (getFirstPlay() === 0) {
+      console.log('player move!');
+    } else {
       aiMove();
     }
     
   }
   
-  // starts the game
-  $('#play').click(function () {
-    newGame();
+  function aiMove() {
+    console.log('ai move!');
+    
+    var empties = [];
+    for (var i = 0; i < 9; i++) {
+      if (board[i] === '') {
+        empties.push(i);
+      }
+    }
+    
+    var index = Math.floor(Math.random() * empties.length);
+    console.log('empties: ' + empties);
+    console.log('empties['+ index + ']: ' + empties[index]);
+    console.log(ai);
+    setTimeout(function () {
+      $('#' + empties[index]).text(ai);
+    }, 200);
+    
+    if (isWin()) {
+      $('#win').text('AI Won!');
+    }
+  }
+  
+  // players move clicks a square
+  $('.square').click(function () {
+      if ($(this).text() === '') {
+        var id = $(this).attr('id');
+        
+        // add move to board
+        board[id] = 'X';
+        console.log(player);
+        $(this).text(player);
+      }
+      console.log(board);
+    
+      if (isWin()) {
+        $('#win').text('You Won!');
+      } else {
+        aiMove();
+      }
+    });
+  
+  $('#start').click(function () {
+    reset();
   });
   
+  // starts the game
+  $('#play').click(function () {
+    playGame();
+  });  
   
-  // player picks what token to play with
+  // Modal open and player picks what token to play with
   $('.btn-lg').click(function () {
     if ($(this).attr('id') === 'X') {
       player = 'X';
+      ai = 'O';
     } else {
       player = 'O';
+      ai = 'X';
     }
     console.log("you choose: " + player);
-  });
-  
-  $('#start').click(function () {
-    if ($('#start').text() === 'Start Over') {
-      reset();
-    }
   });
     
 });
